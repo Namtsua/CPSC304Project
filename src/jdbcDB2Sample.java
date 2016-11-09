9c6777db0c812fa0ee18e9719a009dd53b83e1be
Spencer Austman <spencerlgaustman@gmail.com>
2016-11-08 21:26:32 -0800

    Connection between Bootstrap and Java, basic login page and added needed files

--- src/jdbcDB2Sample.java
+++ src/jdbcDB2Sample.java
@@ -1,10 +1,84 @@
 import java.sql.*; 
 
+import com.teamdev.jxbrowser.chromium.Browser;
+import com.teamdev.jxbrowser.chromium.JSValue;
+import com.teamdev.jxbrowser.chromium.events.FinishLoadingEvent;
+import com.teamdev.jxbrowser.chromium.events.LoadAdapter;
+import com.teamdev.jxbrowser.chromium.events.ScriptContextAdapter;
+import com.teamdev.jxbrowser.chromium.events.ScriptContextEvent;
+import com.teamdev.jxbrowser.chromium.swing.BrowserView;
+
+import javax.swing.*;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.util.concurrent.atomic.AtomicReference;
+
 class jdbcDB2Sample 
 {
     public static void main(String argv[]) 
     {
-       Connection con = null;
+    	final Browser browser = new Browser();
+    	BrowserView browserView = new BrowserView(browser);
+    	final JFrame frame = new JFrame("MediaWiki"); 	
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.add(browserView, BorderLayout.CENTER);
+        frame.setSize(700, 500);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+        
+//        browser.addLoadListener(new LoadAdapter() {
+//            @Override
+//            public void onFinishLoadingFrame(FinishLoadingEvent event) {
+//                if (event.isMainFrame()) {
+//                    JSValue value = browser.executeJavaScriptAndReturnValue("window");
+//                    this.helloWorld();
+//                }
+//            }
+//        });
+        JSValue window = browser.executeJavaScriptAndReturnValue("window");
+        window.asObject().setProperty("java", new JavaObject());
+        browser.addScriptContextListener(new ScriptContextAdapter() {
+            @Override
+            public void onScriptContextCreated(ScriptContextEvent event) {
+                Browser browser = event.getBrowser();
+                JSValue window = browser.executeJavaScriptAndReturnValue("window");
+                window.asObject().setProperty("java", new JavaObject());
+            }  
+        });
+    	browser.loadURL("file://C:/Users/Spencer/Desktop/CS304/CPSC304Project/src/GUI/index.html");
+   // 	connectToDB.addActionListener(new ActionListener() {
+   // 		@Override
+   // 		public void actionPerformed(ActionEvent e) {
+   // 			System.out.println("hello world");
+   // 		}
+   // 		
+   // 	});	
+//    	JPanel contentPane = new JPanel();
+////    	contentPane.add(connectToDB);
+//    	 frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+//         frame.add(contentPane, BorderLayout.CENTER);
+//         frame.setSize(300, 75);
+//         frame.setLocationRelativeTo(null);
+//         frame.setVisible(true);
+    }
+    	
+    public static class JavaObject {
+        public void print(String message) {
+            System.out.println(message);
+        }
+    }
+    	
+    	
+    private static void connection(JFrame parent) {
+       final JDialog dialog = new JDialog(parent, "New User", true);
+       
+       final Browser browser = new Browser();
+       
+     Connection con = null;
        
        try
        {
